5d5f18cd716c5a8f336302257ef24ae593093417
Patrick Rodrigues <47316946+rodpa715@users.noreply.github.com>
2019-10-23 11:01:14 +0200

    add picture URL field

--- database/seeds/01-users.js
+++ database/seeds/01-users.js
@@ -5,6 +5,7 @@ exports.seed = function(knex) {
       id: 1,
       name: 'testuser',
       email: 'testuser@example.com',
+      pictureURL: "https://static.toiimg.com/thumb/msid-69857013,width-640,resizemode-4/69857013.jpg",
       password: '$2a$10$GO/CIkLWX153JsDVw9nom.F41OGx7BGnfXv36Tjzot3zN12QpuPvK'
     },
   ]);
